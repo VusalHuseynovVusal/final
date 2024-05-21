d145a4b7b23179e62ef3826e966d6d0510ce06cc
VusalHuseynovVusal <vusalhuseynov808@gmail.com>
2024-05-21 12:49:13 +0400

    header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,17 @@
-// const xhr = new XMLHttpRequest();
-// xhr.onreadystatechange = () => {
-//   if (xhr.readyState === 4 && xhr.status === 200) {
-//     console.log(xhr.responseText);
-//   }
-// };
-// xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
-// xhr.send();
+function copyMenu() {
+  // copy inside .dpt-cat to .departments
+  var dptCategory = document.querySelector(".dpt-cat");
+  var dptPlace = document.querySelector(".departments");
+  dptCategory.innerHTML = dptPlace;
 
-// let check = 1;
-// const promise = new Promise((resolve, reject) => {
-//   if (check) {
-//     resolve("Promise is successfully");
-//   } else {
-//     reject("Promise is rejected");
-//   }
-// });
-// promise
-//   .then((res) => console.log(res))
-//   .catch((err) => console.log(err))
-//   .finally(() => console.log("done"));
+  //   copy inside nav to nav
+  var mainNav = document.querySelector(".header-nav nav");
+  var navPlace = document.querySelector(".off-canvas nav");
+  mainNav.innerHTML = navPlace.innerHTML;
 
-// function getData(url) {
-//   let promise = fetch(url);
-//   return promise
-//     .then((res) => res.json())
-//     .then((data) => console.log(data))
-//     .catch((err) => console.log(err));
-// }
-// console.log(getData("https://jsonplaceholder.typicode.com/posts"));
+  //   copy .header-top .wrapper to .thetop-nav
+  var topNav = documnet.querySelector(".header-top");
+  var topPlace = documnet.querySelector(".off-canvas .thetop-nav");
+  topNav.innerHTML = topPlace.innerHTML;
+}
+copyMenu();
